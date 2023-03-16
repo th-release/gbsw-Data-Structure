872314723cf535bc6a80c69419c59915c189f6a9
th-release <cth.release@gmail.com>
2023-03-16 09:02:47 +0900

    c++ 출력

---
+++ a2_time/one.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int num = 20;
+  std::cout << "Hello World" << std::endl;
+  std::cout << num << std::endl;
+  std::cout << num << ' ' << 'A' << std::endl;
+  std::cout << ' ' << 3.14 << std::endl;
+
+  return 0;
+}
