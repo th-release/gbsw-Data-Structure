065650946cd6efaa678b88f16ef772085109650e
th-release <cth.release@gmail.com>
2023-03-16 10:02:10 +0900

    구조체 안에 함수 삽입하기

---
+++ a3_time/structFunction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Car {
+  char carName[20];
+  int carGas;
+  int carSpeed;
+  
+  void ShowCar(Car myCar) {
+    cout << "소유자 : " << myCar.carName << endl;
+    cout << "연료량 : " << myCar.carGas << endl;
+    cout << "속도 : " <<  myCar.carSpeed << endl;
+  }
+};
+
+int main() {
+  Car orlando = {"choi", 100, 99};
+  orlando.ShowCar(orlando);
+}
