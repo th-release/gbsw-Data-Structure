6065a12fbdf6a89958b0004fa0f910891deac23c
th-release <cth.release@gmail.com>
2023-03-16 09:17:47 +0900

    using namespace를 써보자

---
+++ a3_time/using.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num = 20;
+  cout << "hello world" << endl;
+  cout << "hello " << "world" << endl;
+  cout << num << ' ' << 'A';
+  cout << ' ' << 3.14 << endl;
+}
